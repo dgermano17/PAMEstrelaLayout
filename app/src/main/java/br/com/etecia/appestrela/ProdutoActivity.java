3e48cc07bcad05c03d4c7b218ce93213355fc41a
LABORATORIO_INFO <danilo.germano@etec.sp.gov.br>
2019-05-22 22:12:06 -0300

    INICIAL + Toolbar

---
+++ app/src/main/java/br/com/etecia/appestrela/ProdutoActivity.java
@@ -0,0 +1,13 @@
+package br.com.etecia.appestrela;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProdutoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.produto_layout);
+    }
+}
