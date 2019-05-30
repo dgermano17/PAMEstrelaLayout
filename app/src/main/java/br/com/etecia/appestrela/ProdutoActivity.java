35c2f836e14b0ef8ba76c29f3b9d4d2e27bfe4b6
dgermano17 <danilo.germano@etec.sp.gov.br>
2019-05-29 22:14:11 -0300

    Menu TOOLBAR CRIADO;

--- app/src/main/java/br/com/etecia/appestrela/ProdutoActivity.java
+++ app/src/main/java/br/com/etecia/appestrela/ProdutoActivity.java
@@ -2,6 +2,7 @@ package br.com.etecia.appestrela;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class ProdutoActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class ProdutoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.produto_layout);
+
+        Toolbar toolbar=(Toolbar) findViewById(R.id.idToolbar);
+        setContentView(R.layout.produto_layout);
+
+        getSupportActionBar().setIcon(R.drawable.ic_arrow_back_dp);
+        getSupportActionBar().setTitle("EstrelaApp");
     }
 }
